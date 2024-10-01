08284cdb65e64089cfe36de31e7b96889207dca8
S Harshavardhan <harsha.echo348@gmail.com>
2024-10-01 23:53:33 +0530

    Add files via upload

---
+++ 1-hello_world.py
@@ -0,0 +1,8 @@
+# Print hello world using python
+
+#1
+print("hello_world")
+
+#2
+a="hello_world"
+print(a)
---
+++ 2-calculator.py
@@ -0,0 +1,17 @@
+# Normal calculator to add, subtract, multiply, and divide two numbers
+
+# Get user input:
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Perform operations:
+add = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2 
+
+# Print results
+print(f"Sum: {add}")
+print(f"Difference: {subtract}")
+print(f"Product: {multiply}")
+print(f"Quotient: {divide}")
