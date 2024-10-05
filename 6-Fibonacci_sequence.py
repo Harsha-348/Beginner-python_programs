319f87031478803ff6f8d0e8f4777dccfe9db7d0
S Harshavardhan <harsha.echo348@gmail.com>
2024-10-05 18:38:15 +0530

    Add files via upload

---
+++ 6-Fibonacci_sequence.py
@@ -0,0 +1,14 @@
+# # Program to print Fibonacci sequence up to n-th term
+n = int(input("enter the no of terms:"))
+
+a=0
+b=1
+print("fibonacci sequence")
+for _ in range(n):
+    print(a)
+    a, b = b, a + b
+
+
+
+
+
