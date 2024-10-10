74140f37b5a71a823465cd1c917c0a8b7980055c
S Harshavardhan <harsha.echo348@gmail.com>
2024-10-10 15:25:05 +0530

    Add files via upload

---
+++ 12-Find_Smallest_Num.py
@@ -0,0 +1,5 @@
+# Program to find the smallest number in a list
+num1=int(input("enetr the number:"))
+num2=int(input("enetr the number:"))
+small= min(num1,num2)
+print(f"The smallest number is {small}")
---
+++ 13-Swap_Variables.py
@@ -0,0 +1,8 @@
+# Program to swap two variables
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Swap
+a, b = b, a
+
+print(f"After swapping: a = {a}, b = {b}")
