ccaa16e95efd7060e80c83dbeee24d94f01805e7
S Harshavardhan <harsha.echo348@gmail.com>
2024-10-11 15:52:53 +0530

    Add files via upload

---
+++ 14-Celsius-Fahrenheit.py
@@ -0,0 +1,4 @@
+# Program to convert Celsius to Fahrenheit
+celsius = float(input("Enter temperature in Celsius: "))
+fahren = (celsius * 9/5) + 32
+print(f"{celsius}°C is {fahren}°F")
---
+++ 15-Fahrenheit-Celsius.py
@@ -0,0 +1,4 @@
+# Program to convert Fahrenheit to Celsius
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+print(f"{fahrenheit}°F is {celsius}°C")
