46ea1f256cdb9c86bff558c0f6a62f52c4b632f2
S Harshavardhan <harsha.echo348@gmail.com>
2024-10-17 14:24:50 +0530

    Update 20-Kilometers_to_Miles.py

--- 20-Kilometers_to_Miles.py
+++ 20-Kilometers_to_Miles.py
@@ -2,5 +2,4 @@
 kilometers = float(input("Enter distance in kilometers: "))
 miles = kilometers * 0.621371
 print(f"{kilometers} km is {miles} miles")
-
 #This program converts kilometers to miles using the conversion factor `1 km = 0.621371 miles`.
