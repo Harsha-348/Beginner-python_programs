2b2c1303d7cf44cb928e01b4489d1b2f67667f3d
S Harshavardhan <harsha.echo348@gmail.com>
2024-10-18 14:23:58 +0530

    Create 22-ASCII_Value.py

---
+++ 22-ASCII_Value.py
@@ -0,0 +1,7 @@
+# Program to find ASCII value of a character
+char = input("Enter a character: ")
+ascii_value = ord(char)
+print(f"ASCII value of {char} is {ascii_value}")
+
+'''This program uses the ord() function 
+to find the ASCII value of a character.'''
