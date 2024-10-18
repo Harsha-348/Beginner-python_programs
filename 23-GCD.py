59b3a102d8745a10077fef08fcf561fb4895ae28
S Harshavardhan <harsha.echo348@gmail.com>
2024-10-18 14:25:46 +0530

    Create 23-GCD.py

---
+++ 23-GCD.py
@@ -0,0 +1,10 @@
+# Program to find GCD of two numbers
+import math
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+gcd = math.gcd(num1, num2)
+print(f"GCD of {num1} and {num2} is {gcd}")
+
+'''This program uses the math.gcd() function 
+to find the greatest common divisor (GCD) of two numbers.'''
