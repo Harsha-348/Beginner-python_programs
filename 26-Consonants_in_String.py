afd13b3ec098a0bf5f7789c1152760d61731544b
S Harshavardhan <harsha.echo348@gmail.com>
2024-10-21 23:58:04 +0530

    Create 26-Consonants_in_String.py

---
+++ 26-Consonants_in_String.py
@@ -0,0 +1,11 @@
+# Program to count consonants in a string
+string = input("Enter a string: ").lower()
+vowels = "aeiou"
+count = 0
+
+for char in string:
+    if char.isalpha() and char not in vowels:
+        count += 1
+print(f"Number of consonants in the string is {count}")
+
+'''This program counts the number of consonants in a string by iterating through each character.'''
